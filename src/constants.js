d9d3987714c79313d201a9a350754208c0e0268b
jimyandres <jimyandres@utp.edu.co>
2018-04-03 09:44:08 -0500

    Minor bug fixed

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ SELECT
 	prog_codigo,
 	prog_nombre,
 	prog_version,
-	CASE WHEN prog_web=false THEN 'Web' ELSE 'Desktop' END AS prog_type,
+	CASE WHEN prog_web=true THEN 'Web' ELSE 'Desktop' END AS prog_type,
 	CASE WHEN prog_desarrollador ='0' THEN 'Not asigned' ELSE (
     SELECT usua_nombre FROM seguridad.usuario WHERE usua_documento = prog_desarrollador LIMIT 1
   ) END AS prg_desarrollador
--- src/readme.template.js
+++ src/readme.template.js
@@ -1,11 +1,10 @@
 module.exports = function (project) {
   const { code, name, ver, type, lastDev, location } = project;
-  const app_type = type === 'D' ? 'Desktop' : 'Web';
   return `
 Cod: ${code}\r\n
 Name: ${name}\r\n
 Version: ${ver}\r\n
-Type: ${app_type}\r\n
+Type: ${type}\r\n
 Last_Dev: ${lastDev}\r\n
 Location: ${location}`;
 };
